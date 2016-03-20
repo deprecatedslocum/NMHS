07348a55c08e2ac301e001b27a82e8a36249df8e
Danil Tyulmankov <danil@MIT.EDU>
2016-03-20 16:39:57 -0400

    changed 'connectivity' to 'interactivity'

---
+++ connectivity/checkDims2d.m
@@ -0,0 +1,31 @@
+function [N, M, J, K] = checkDims2d(tr1, tr2, e1, e2)
+%tr1 is NxNxM, tr2 is MxMxN, e1 is NxJ, e2 is MxK
+%
+% Authors: Danil Tyulmankov, Joshua Slocum, Alexander Friedman
+% Copyright 2016 Danil Tyulmankov, MIT
+
+if ndims(tr1) ~= 3 || ndims(tr2) ~= 3
+    error('Transition matrices must both be 3-dimensional')
+end
+
+[N1, N2, M] = size(tr1);
+[M1, M2, N] = size(tr2);
+if N ~= N1 || N ~= N2 || N1 ~= N2 || M ~= M1 || M ~= M2 || M1 ~= M2
+    error('Input matrix size mismatch. tr1 must be NxNxM, tr2 must be MxMxN.')
+end
+
+if nargin == 4
+    if ~ismatrix(e1) || ~ismatrix(e2)
+        error('Emission matrices must both be 2-dimensional')
+    end
+    
+    [N1, J] = size(e1);
+    [M1, K] = size(e2);
+    if N ~= N1 || M ~= M1
+        error('Input matrix size mismatch. tr1 must be NxNxM, tr2 must be MxMxN, e1 must be NxJ, e2 must be MxK.')
+    end
+    
+    if N > J || M > K
+        error('Input matrix size mismatch. Cannot have more states than emissions.')
+    end
+end
---
+++ connectivity/interactivity.m
@@ -0,0 +1,33 @@
+function c = interactivity(tr1, tr2, tr3)
+% Wrapper for interactivity2d.m and interactivity3d.m Returns a summary of 
+% pairwise interactivities. Entry c(i,j) is "j affects i"
+%
+% Authors: Danil Tyulmankov, Joshua Slocum, Alexander Friedman
+% Copyright 2016 Danil Tyulmankov, MIT
+
+
+if nargin == 3
+    c = zeros(3);
+    
+    [c12, c13] = interactivity3d(tr1, tr2, tr3, false);
+    [c31, c32] = interactivity3d(tr3, tr1, tr2, false);
+    [c23, c21] = interactivity3d(tr2, tr3, tr1, false);
+    
+    c(1,2) = mean(c12);
+    c(2,1) = mean(c21);
+    c(1,3) = mean(c13);
+    c(3,1) = mean(c31);
+    c(2,3) = mean(c23);
+    c(3,2) = mean(c32);
+    
+elseif nargin == 2
+    c = zeros(2);
+    
+    c(1,2) = interactivity(tr1, tr2, false);
+    c(2,1) = interactivity(tr2, tr1, false);
+    
+else
+    error('Incorrect number of arguments')
+    
+end
+
